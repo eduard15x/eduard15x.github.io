c967ff259464cbe896df2c29ee81020d29a565c5
Precup Eduard <precupeduard99@gmail.com>
2022-05-13 18:07:40 +0300

    Update portof / Finish responsive design

--- scripts/personalProjects.js
+++ scripts/personalProjects.js
@@ -16,9 +16,11 @@ const getJSON = async url => {
     const btnNextProject = document.getElementById('next-project');
 
 getJSON('data/personalProjects.json').then( projects => {
+
     let index = 0
-    function displayProjects() {
-        for ( let i = index; i < index + 3; i++) {
+
+    function displayProjects(nrProjects) {
+        for ( let i = index; i < index + nrProjects; i++) {
             ulList.innerHTML += `
             <li class="li-project animation">
                 <h4 class="projects-h4">${projects[i].name}</h4>
@@ -38,43 +40,63 @@ getJSON('data/personalProjects.json').then( projects => {
         e.style.cursor = "default"
     }
 
-    
+    function displayOtherProjects(stopPrev, stopNext, xProjects) {
+        btnPrevProject.disabled = true;
+        btnPrevProject.addEventListener('click', ()=> {
+            
+            if ( index === stopPrev) {
+                disabledBtn(btnPrevProject)
+                return
+            } else {
+                index--;
+                enabledBtn(btnPrevProject)
+                enabledBtn(btnNextProject)
+            };
+            ulList.innerHTML = ''
+            displayProjects(xProjects)
+            console.log(index)
+        });
 
-
-    btnNextProject.addEventListener('click', ()=> {
-        if ( index < projects.length - 3 ) {       
-            index++;    
-            enabledBtn(btnNextProject)
-            enabledBtn(btnPrevProject)
-        } else {
-            disabledBtn(btnNextProject)
-            return
-        };
-        ulList.innerHTML = ''
-        displayProjects()
-    });
-
-    btnPrevProject.disabled = true;
-    btnPrevProject.addEventListener('click', ()=> {
-        if ( index === 0) {
-            disabledBtn(btnPrevProject)
-            return
-        } else {
-            index--;
-            enabledBtn(btnPrevProject)
-            enabledBtn(btnNextProject)
-        };
-        ulList.innerHTML = ''
-        displayProjects()
-    });   
-
-
-    displayProjects()
+        btnNextProject.addEventListener('click', ()=> {
+            
+            if ( index < projects.length - stopNext ) {       
+                index++;    
+                enabledBtn(btnNextProject)
+                enabledBtn(btnPrevProject)
+            } else {
+                disabledBtn(btnNextProject)
+                return
+            };
+            ulList.innerHTML = ''
+            displayProjects(xProjects)
+            console.log(index)
+        });
+    }
     disabledBtn(btnPrevProject)
 
-    
 
-    
+
+    //creating function to display 1 project for mobile version or 3 projects for desktop version
+    function myFunction(x) {
+        ulList.innerHTML = ''
+        if (x.matches) { // If media query matches
+            ulList.innerHTML = ''
+            displayProjects(1)
+            displayOtherProjects( 0 , 1 , 1 )
+            
+        } else {
+            ulList.innerHTML = ''
+            displayProjects(3)
+            displayOtherProjects( 0 , 3 , 3 )
+        }
+        index = 0
+    }
+
+    myFunction(window.matchMedia("(max-width: 600px)")) // Call listener function at run time
+    window.matchMedia("(max-width: 600px)").addListener(myFunction) // Attach listener function on state changes
+
+
+
 }).catch(error => {
     console.error(error)
 })
--- scripts/script.js
+++ scripts/script.js
@@ -1,44 +1,3 @@
-
-
-
-
-
-
-// //burger button for mobile layout
-
-// const menuBtn = document.querySelector('.menu-burger');
-// const navbarBurger = document.querySelector('.navbar-burger');
-// let menuIsOpen = false;
-
-
-
-// menuBtn.addEventListener('click', ()=> {
-//     if(!menuIsOpen) {
-//         menuBtn.classList.add('open');
-//         menuIsOpen = true;
-//         navbarBurger.style.display = 'flex';
-//     } else {
-//         menuBtn.classList.remove('open');
-//         menuIsOpen = false;
-//         navbarBurger.style.display = 'none';
-//     }
-// })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 //Change navbar items color when you scroll out of the intro section
 
 //variant 1
@@ -54,34 +13,77 @@
 // }
 
 
-//variant 2 with jQuery
-
-// $(window).scroll( () => {
-//     if ( $(window).scrollTop() > $(window).height() ) {
-//         $('.header-section').css({'background-color' : 'blue'})
-//     } else {
-//         $('.header-section').css({'background-color' : 'red'})
-//     }
-// } )
-
-
 
 
 let iconHome = document.querySelector('#icon-home i')
 let navLinks = document.querySelectorAll('.nav-bar-header a')
 let navbar = document.querySelector('.header-section')
-
 let heightToChange = window.innerHeight - 40    
 
-window.onscroll = function() {
-    function changeColors() {
-        if ( window.scrollY > heightToChange) {
-            navbar.style.backgroundColor = 'var(--header-color)'
-        } else {
-            navbar.style.backgroundColor = 'transparent'
+function changeHeaderOnscroll(height) {
+    window.onscroll = function() {
+        function changeColors() {
+            if ( window.scrollY > height) {
+                navbar.style.backgroundColor = 'var(--text-color)'
+                openMenu.style.color = 'var(--text-color)'
+                iconHome.style.color = 'var(--hover-color)'
+            } else {
+                if ( (window.matchMedia("(max-width: 600px)").matches) ) {
+                    navbar.style.backgroundColor = 'var(--header-color)'    
+                } else {
+                    navbar.style.backgroundColor = 'transparent'
+                }
+                openMenu.style.color = 'var(--section-color)'
+                iconHome.style.color = 'var(--section-color)'
+            }
         }
+        changeColors()
     }
-
-    changeColors()
-    
 }
+
+
+//burger
+const openMenu = document.querySelector('#open-nav-menu')
+const closeMenu = document.querySelector('#close-nav-menu')
+const navbarLinks = document.querySelector('.nav-bar-header')
+const mainSection = document.querySelector('main')
+
+
+openMenu.addEventListener('click', () => {
+    closeMenu.style.display = 'block'
+    openMenu.style.display = 'none'
+    navbarLinks.style.display = 'flex'
+    mainSection.style.paddingTop = '120px'
+})
+
+closeMenu.addEventListener('click', () => {
+    closeMenu.style.display = 'none'
+    openMenu.style.display = 'block'
+    navbarLinks.style.display = 'none'
+    mainSection.style.paddingTop = '0px'
+})
+
+
+
+
+
+
+//Media query for nav bar and burger-menu
+
+function myFunction(x) {
+    if (x.matches) { // If media query matches
+        openMenu.style.display = 'block'
+        closeMenu.style.display = 'none'
+        navbarLinks.style.display = 'none'
+        changeHeaderOnscroll(heightToChange - 175)
+
+    } else {
+        openMenu.style.display = 'none'
+        closeMenu.style.display = 'none'
+        navbarLinks.style.display = 'flex'
+        changeHeaderOnscroll(heightToChange)
+    }
+}
+  
+myFunction(window.matchMedia("(max-width: 600px)")) // Call listener function at run time
+window.matchMedia("(max-width: 600px)").addListener(myFunction) // Attach listener function on state changes
